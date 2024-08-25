003a963e8f38625f40c317102ac3387e884fc2e5
ankit0511 <99800154+ankit0511@users.noreply.github.com>
2024-08-25 12:10:28 +0530

    Api Erros and Responces

--- src/index.js
+++ src/index.js
@@ -1,32 +1,21 @@
-
-import  dotenv  from "dotenv"
+import dotenv from "dotenv";
 
 import connectDB from "./db/index.js";
-import express from "express"
+import express from "express";
 const app = express();
 
-
 dotenv.config({
-    path:'./env'
-})
-connectDB();
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+  path: "./env",
+});
+connectDB()
+  .then(() => {
+    app.listen(process.env.PORT || 8000, () => {
+      console.log(`Server is running on the port ${process.env.PORT}`);
+    });
+  })
+  .catch((error) => {
+    console.log("connection to the database is Successfull ", error);
+  });
 
 /*
 
