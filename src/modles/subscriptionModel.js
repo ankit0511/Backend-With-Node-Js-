872ec1f295a9bd0cafd62811dc1ca44fb784d1c6
ankit0511 <99800154+ankit0511@users.noreply.github.com>
2024-08-29 14:14:33 +0530

    Subscription Schema

---
+++ src/modles/subscriptionModel.js
@@ -0,0 +1,15 @@
+
+import mongoose, {model, Schema} from "mongoose";
+
+const subscriptionSchema = new Schema({
+subscriber:{
+    type :Schema.Types.ObjectId,
+    ref:"User"
+},
+channel:{
+    type: Schema.Types.ObjectId,
+    ref:"User"
+}
+})
+
+export const Subscription = model("Subscription",subscriptionSchema)
